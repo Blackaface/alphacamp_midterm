17d07bf893c892e1e818a429ce44c98483cbe48f
Fang Ernesto <zxc9799979@gmail.com>
2016-11-18 17:05:34 +0800

    initial crud

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,2 +1,45 @@
 class DisastersController < ApplicationController
+  def index 
+    @disasters = Disaster.all
+  end
+
+  def new
+    @disaster = Disaster.new
+  end
+
+  def create
+    @disaster = Disaster.new(disaster_params)
+    @disaster.save
+
+    redirect_to disasters_path
+  end
+
+  def show
+    @disaster = Disaster.find(params[:id])
+  end
+
+  def edit
+    @disaster = Disaster.find(params[:id])
+  end
+
+  def update
+    @disaster = Disaster.find(params[:id])
+    @disaster.update(disaster_params)
+
+    redirect_to disaster_path(@disaster)
+  end
+
+  def destroy
+    @disaster = Disaster.find(params[:id])
+    @disaster.destroy
+
+    redirect_to :action => :index
+  end
+
+  private
+
+  def disaster_params
+    params.require(:disaster).permit(:title, :content)
+  end
+
 end
--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -1,2 +1,3 @@
 class Disaster < ApplicationRecord
+  
 end
