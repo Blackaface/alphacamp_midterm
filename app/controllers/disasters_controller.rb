135486761d88a98320e0d70962126e8d23b54c44
Fang Ernesto <zxc9799979@gmail.com>
2016-11-18 18:04:06 +0800

    devise set up

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,4 +1,5 @@
 class DisastersController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
   before_action :set_disaster, :only => [ :show, :edit, :update, :destroy]
 
   def index 
--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -1,3 +1,4 @@
 class Disaster < ApplicationRecord
+  belongs_to :user
   validates_presence_of :title , :content
 end
